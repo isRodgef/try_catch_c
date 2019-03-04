a50a02e934161472565907779bb4a3c0648eeb50
rodgef <rodgef101@gmail.com>
2019-03-04 11:40:25 +0200

    added toexecute to try statment

--- src/cexecption.c
+++ src/cexecption.c
@@ -7,12 +7,14 @@ void catch(void (*get_fun_ptr)(exception), exception e)
 	get_fun_ptr(e);
 }
 
-void try(int cond,void (*set_fun_ptr)(exception *), void (*get_fun_ptr)(exception), exception* e)
+void try(int cond,void (*to_execute) ,void (*set_fun_ptr)(exception *), void (*get_fun_ptr)(exception), exception* e)
 {
 	if (cond){
 		set_fun_ptr(e);
 		catch(get_fun_ptr, *e);
 	}
+	else
+		to_execute();
 		
 		
 }
